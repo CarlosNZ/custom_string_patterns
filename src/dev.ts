0a8c6641cc11477a5242c1313b0b60c0625654c2
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-07-26 00:42:33 +1200

    Better way to handle escaped <> without lookbehind/lookahead

--- src/dev.ts
+++ src/dev.ts
@@ -3,50 +3,15 @@ This file used as a playground for development. It is not published in the actua
 */
 
 import Pattern, { patternGen } from './patterns'
-import fetch from 'node-fetch'
-const checkdigit = require('checkdigit')
 
-// Function to fetch an item from an online API
-const getAlbumString = async (key: number) => {
-  const data = await fetch('https://jsonplaceholder.typicode.com/albums')
-  return (await data.json())[key].title
-}
+// const basicPattern = new Pattern(/.{10}-<+ddd>/i)
 
-const basicPattern = new Pattern(/.{100}/i)
+// basicPattern.gen().then((res) => console.log(res))
 
-basicPattern.gen().then((res) => console.log(res))
+// const patternWithLiterals = new Pattern(/\<1\>-[A-Z]{3}-<+dd>-<?f1>/, {
+//   customReplacers: { f1: () => 'Testing' },
+// })
 
-const fancyPattern = new Pattern(/Album name: <?album>, serial: [A-Z]{3}_<+d> \(<?upper>\)/, {
-  counterInit: 5000,
-  counterIncrement: (prev) => Number(prev) + 100,
-  customReplacers: {
-    album: getAlbumString,
-    upper: (str: string) => str.toUpperCase(),
-  },
-  numberFormat: new Intl.NumberFormat('en-US'),
-})
+// patternWithLiterals.gen().then((res) => console.log(res))
 
-fancyPattern
-  .gen({
-    customArgs: {
-      album: 10,
-      upper: '_end',
-    },
-  })
-  .then((res) => console.log(res))
-
-const dynamicArgPattern = new Pattern(/([a-z]{3,6})-(test)-<+ddd>-<?upper(1, 2)>-<?lower>/, {
-  customReplacers: {
-    upper: (...args: string[]) => args.join('').toUpperCase(),
-    lower: (chars: string) => chars.toLowerCase(),
-  },
-})
-
-const showDynamicArgPattern = async () => {
-  console.log('Testing passing capture groups to customReplacers')
-  for (let i = 1; i < 12; i++) {
-    console.log('Output:', await dynamicArgPattern.gen({ customArgs: { lower: 'SomThiNG' } }))
-  }
-}
-
-showDynamicArgPattern()
+patternGen(/Another\<<+dd>-\>\>_<+dddd>_DONE/).then((res) => console.log(res))
