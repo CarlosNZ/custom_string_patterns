a7ec0937269c08aa67b2f99095fb6f067a896645
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-06-05 09:37:19 +1200

    Update database.ts

--- demo/src/database/database.ts
+++ demo/src/database/database.ts
@@ -1,28 +1,10 @@
-import {
-  getFirestore,
-  runTransaction,
-  doc,
-  getDoc,
-  updateDoc,
-  FieldValue,
-} from 'firebase/firestore'
+import { getFirestore, runTransaction, doc, getDoc } from 'firebase/firestore'
 import { initializeApp } from 'firebase/app'
 import firebaseConfig from '../secrets/firebaseConfig.json'
 
 const app = initializeApp(firebaseConfig)
 
 const db = getFirestore(app)
-
-// export const getCounter = async (counterName: string) => {
-//   const counterRef = doc(db, 'counters', counterName)
-
-//   // const counterRef =
-//   // const counterRef = db.collection('cities').doc('DC');
-
-//   const count = await updateDoc(counterRef, { count: 200 })
-//   console.log('Count', count)
-//   return count
-// }
 
 export const getCounter = async (counterName: string) => {
   const counterRef = doc(db, 'counters', counterName)
